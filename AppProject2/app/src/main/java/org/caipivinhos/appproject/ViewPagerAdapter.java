b34f5b088dec43db148084f15f59221603a2a77a
Jose Almeida <zepintoalmeida@gmail.com>
2022-11-29 15:57:55 +0000

    Updated Login Screen and added pie chart

--- AppProject2/app/src/main/java/org/caipivinhos/appproject/ViewPagerAdapter.java
+++ AppProject2/app/src/main/java/org/caipivinhos/appproject/ViewPagerAdapter.java
@@ -9,31 +9,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import java.util.Random;
+
 public class ViewPagerAdapter extends PagerAdapter{
 
-    private Context context;
-    private String[] recs = {"ola", "adeus"};
+    private final Context context;
+    private String[] recs = new String[5];
     private LayoutInflater layoutInflater;
 
     public ViewPagerAdapter(Context context) {
         this.context = context;
     }
 
+    public void setString() {
+        String[] tipsArray;
+
+        tipsArray = context.getResources().getStringArray(R.array.tips);
+
+        for(int i = 0; i<5 ; i++) {
+            int randomIndex = new Random().nextInt(16);
+            recs[i] = tipsArray[randomIndex];
+
+        }
+    }
     @Override
     public int getCount() {
         return recs.length;
     }
 
     @Override
-    public boolean isViewFromObject(View view, Object object) {
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return view == object;
     }
 
+    @NonNull
     @Override
-    public Object instantiateItem(ViewGroup container, final int position) {
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        setString();
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.rec_viewer, null);
         TextView textView = (TextView) view.findViewById(R.id.textViewer);
@@ -45,7 +61,7 @@ public class ViewPagerAdapter extends PagerAdapter{
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
         ViewPager vp = (ViewPager) container;
         View view = (View)object;
         vp.removeView(view);
