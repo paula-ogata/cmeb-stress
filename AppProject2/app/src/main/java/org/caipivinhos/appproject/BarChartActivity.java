473c1290c89c84d5fdced941da8409c6a6cff662
Jose Almeida <zepintoalmeida@gmail.com>
2022-11-29 14:58:20 +0000

    Updated Login Screen and added pie chart

---
+++ AppProject2/app/src/main/java/org/caipivinhos/appproject/BarChartActivity.java
@@ -0,0 +1,50 @@
+package org.caipivinhos.appproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+
+public class BarChartActivity extends AppCompatActivity {
+    ArrayList barArraylist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_chart);
+
+        BarChart barChart = findViewById(R.id.barchart);
+        // Adds data to the barArraylist
+        getData();
+        BarDataSet barDataSet = new BarDataSet(barArraylist,"Stress Levels");
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        //color bar data set
+        barDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        //text color
+        barDataSet.setValueTextColor(Color.BLACK);
+        //settting text size
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(false);
+    }
+
+    private void getData()
+    {
+        barArraylist = new ArrayList();
+        barArraylist.add(new BarEntry(2f,10));
+        barArraylist.add(new BarEntry(3f,10));
+        barArraylist.add(new BarEntry(4f,30));
+        barArraylist.add(new BarEntry(5f,25));
+        barArraylist.add(new BarEntry(6f,50));
+
+    }
+}
